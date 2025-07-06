f448e6b980eff06c0b09095589d3d51fcd995e96
G-Dimchris <122032502+G-Dimchris@users.noreply.github.com>
2025-07-06 16:12:58 +0100

    Update PrimaryApplicationInstruction.jsx

--- components/form/PrimaryApplicationInstruction.jsx
+++ components/form/PrimaryApplicationInstruction.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-export default function ApplicationInstructions({ onBegin }) {
+export default function PrimaryInstructions({ onBegin }) {
   const [checked, setChecked] = useState(false);
 
   return (
