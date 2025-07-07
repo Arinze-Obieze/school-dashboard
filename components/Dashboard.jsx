1a4e40128669269d5608fb272cf2305ce64d7522
G-Dimchris <122032502+G-Dimchris@users.noreply.github.com>
2025-07-07 22:49:01 +0100

    Update Dashboard.jsx

--- components/Dashboard.jsx
+++ components/Dashboard.jsx
@@ -22,7 +22,7 @@ const Dashboard = () => {
 
   const notifications = [
     { id: 1, message: 'School portal will be under maintenance this weekend.' },
-    { id: 2, message: 'New course materials will been uploaded soon.' },
+    { id: 2, message: 'Get to know your center.' },
     { id: 3, message: 'Deadline for registration is not yet out.' },
   ];
 
