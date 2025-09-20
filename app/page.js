6cd35958dbcd2bc7f4a625de0c5ca5fa067d858e
Arinze <xboss000000@gmail.com>
2025-09-20 17:02:40 +0100

    fixed the layout issue

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
 import Dashboard from "@/app/(dashboard)/dashboard/page"
+import Layout from "./(dashboard)/layout";
 
 export default function Home() {
   return (
-  <>
+  <Layout>
 <Dashboard/>
-  </>
+  </Layout>
   );
 }
