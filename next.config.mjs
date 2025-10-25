c5a0743e689df8bf633f918d4e966c175ae8f5c8
Arinze <xboss000000@gmail.com>
2025-10-25 23:35:50 +0100

    installed zeptomail and fixed the build error

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig =  {
-    experimental: {
-      turbo: false, // Disable Turbopack, fallback to Webpack
-    },
-  }
+
+}
 
 export default nextConfig;
